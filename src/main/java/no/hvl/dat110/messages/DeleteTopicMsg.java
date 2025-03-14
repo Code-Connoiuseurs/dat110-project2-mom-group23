c9324be92f617833653133eddf0e449b4ce06ea1
Simen Strømsnes <simen@simsine.no>
2025-03-14 13:58:30 +0100

    Implement message classes

--- src/main/java/no/hvl/dat110/messages/DeleteTopicMsg.java
+++ src/main/java/no/hvl/dat110/messages/DeleteTopicMsg.java
@@ -4,9 +4,25 @@ public class DeleteTopicMsg extends Message {
 
 	// message sent from client to create topic on the broker
 
-    public DeleteTopicMsg(String user, String topic) {
+	@Override
+	public String toString() {
+		return "DeleteTopicMsg [topic=" + topic + "]";
+	}
 
+	private String topic;
+	
+    public DeleteTopicMsg(String user, String topic) {
+    	super(MessageType.DELETETOPIC, user);
+    	
     }
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
 
 	// TODO:
 	// Implement object variables - a topic is required
